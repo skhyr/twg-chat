6fef2fb2346719c133adbc582fc691d9aae9bce5
skhyr <piotrmal2002@gmail.com>
2021-05-13 21:06:14 +0200

    added dotenv

--- queries/apolloClient.ts
+++ queries/apolloClient.ts
@@ -1,12 +1,13 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { TOKEN } from '@env'
 
 const httpLink = createHttpLink({
   uri: 'https://chat.thewidlarzgroup.com/api/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = "token";
+  const token = TOKEN;
   return {
     headers: {
       ...headers,
